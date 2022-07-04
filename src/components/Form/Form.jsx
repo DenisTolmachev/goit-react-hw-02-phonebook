6d6b1b1219a8d71150fa57a19678609c3cb24b1c
Denis <denis.tolmachev@gmail.com>
2022-07-04 21:00:24 +0300

    add form placeholders

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -37,6 +37,7 @@ export class ContactForm extends Component {
           name="name"
           pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
           title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+          placeholder='Rosie Simpson'
           required
           id={this.nameInput}
           value={this.state.name}
@@ -48,6 +49,7 @@ export class ContactForm extends Component {
           name="number"
           pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
           title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+          placeholder='XXX-XX-XX'
           required
           id={this.numberInput}
           value={this.state.number}
