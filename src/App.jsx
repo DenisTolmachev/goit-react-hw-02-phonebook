aa08a5efc6049de4ed811a6af5d6dff58b020031
Denis <denis.tolmachev@gmail.com>
2022-07-07 20:18:03 +0300

    clearing code

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,17 @@
 import { Component } from 'react';
-import { ContactForm } from 'components/Form/Form';
 import shortid from 'shortid';
+import { ContactForm } from 'components/Form/Form';
 import { ContactList } from 'components/ContactList/ContactList';
 import { Filter } from 'components/Filter/Filter';
-import { Container } from 'App.styled';
-import { Notification } from 'components/commons';
+import { Notification, Container} from 'components/commons';
 
 export class App extends Component {
   state = {
     contacts: [
-      // { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      // { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      // { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      // { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
     ],
     filter: '',
   };
@@ -42,7 +41,7 @@ export class App extends Component {
     console.log(this.state.filter);
   };
 
-  getVisibleContacts = () => {
+  getFilteredContacts = () => {
     const { contacts, filter } = this.state;
     const normalizedContacts = filter.toLowerCase();
     return contacts.filter(contact =>
@@ -52,7 +51,7 @@ export class App extends Component {
 
   render() {
     const { filter, contacts } = this.state;
-    const filteredContacts = this.getVisibleContacts();
+    const filteredContacts = this.getFilteredContacts();
     console.log(contacts.length);
     return (
       <Container>
--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,38 +1,11 @@
 import React, { Component } from 'react';
 import shortid from 'shortid';
-//import NumberFormat from 'react-number-format';
-// import { Formik } from 'formik';
-// import * as yup from 'yup';
 import { Form } from './Form.styled';
-import { Button, Input,InputNumber, Label } from 'components/commons';
-
-// const mySchema = yup.object().shape({
-//   name: yup.string().min(2).required('Name is required'),
-//   number: yup.string().length(9).required('Number is required'),
-// });
-
+import { Button, Input, Label } from 'components/commons';
 export class ContactForm extends Component {
-  // Контрольована форма
-  // state = {
-  //   name: '',
-  //   number: '',
-  // };
 
   nameInput = shortid.generate();
   numberInput = shortid.generate();
-
-  // handleChange = e => {
-  //   const { name, value } = e.currentTarget;
-  //   this.setState({
-  //     [name]: value,
-  //   });
-  // };
-
-  // handleSubmit = e => {
-  //   e.preventDefault();
-  //   this.props.onSubmit(this.state);
-  //   this.reset();
-  // };
 
   handleSubmit = e => {
     e.preventDefault();
@@ -44,18 +17,8 @@ export class ContactForm extends Component {
     form.reset();
   };
 
-  // reset = () => {
-  //   this.setState({ name: '', number: '' });
-  // };
-
   render() {
     return (
-      // <Formik
-      //   initialValues={this.state}
-      //   validationSchema={mySchema}
-      //   onSubmit={this.hadleSubmit}
-      // >
-      // {props => ()};
       <Form onSubmit={this.handleSubmit}>
         <Label htmlFor={this.nameInput}>
           Name
@@ -67,27 +30,21 @@ export class ContactForm extends Component {
             placeholder="Rosie Simpson"
             required
             id={this.nameInput}
-            // value={this.state.name}
-            // onChange={this.handleChange}
           />
         </Label>
         <Label htmlFor={this.numberInput}>
           Number
-          <InputNumber
-            format="+38 (###) ###-##-##"
-            allowEmptyFormatting
-            mask="_"
+          <Input
             type="tel"
             name="number"
+            pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+  title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
             required
             id={this.numberInput}
-            value=""
-            // onChange={this.handleChange}
           />
         </Label>
         <Button type="submit">Add contact</Button>
       </Form>
-      //</Formik>
     );
   }
 }
